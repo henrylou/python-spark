c90e8f2df0791a69a8ea116dc086f663bbad952a
Hengyu Lou <hylou34@gmail.com>
2018-07-26 17:28:33 -0700

    Solve same hosts problem

--- rdd/nasaApacheWebLogs/SameHostsProblem.py
+++ rdd/nasaApacheWebLogs/SameHostsProblem.py
@@ -1,20 +1,33 @@
-from pyspark import SparkContext
+from pyspark import SparkContext, SparkConf
 
 if __name__ == "__main__":
+    conf = SparkConf().setAppName("hostsIntersection").setMaster("local[*]")
+    sc = SparkContext(conf = conf)
 
-	'''
-    "in/nasa_19950701.tsv" file contains 10000 log lines from one of NASA's apache server for July 1st, 1995.
-    "in/nasa_19950801.tsv" file contains 10000 log lines for August 1st, 1995
-    Create a Spark program to generate a new RDD which contains the hosts which are accessed on BOTH days.
-    Save the resulting RDD to "out/nasa_logs_same_hosts.csv" file.
+    julyFirstLogs = sc.textFile("in/nasa_19950701.tsv")
+    augustFirstLogs = sc.textFile("in/nasa_19950801.tsv")
 
-    Example output:
-    vagrant.vf.mmc.com
-    www-a1.proxy.aol.com
-    .....    
+    julyFirstHosts = julyFirstLogs.map(lambda line: line.split('\t')[0])
+    augustFirstHosts = augustFirstLogs.map(lambda line: line.split('\t')[0])
 
-    Keep in mind, that the original log files contains the following header lines.
-    host    logname    time    method    url    response    bytes
+    intersectedLogHosts = julyFirstHosts.intersection(augustFirstHosts)
+    cleanLogHosts = intersectedLogHosts.filter(lambda line: line != 'host')
 
-    Make sure the head lines are removed in the resulting RDD.
-    '''
+    cleanLogHosts.saveAsTextFile("out/nasa_logs_same_hosts.csv")
+
+'''
+"in/nasa_19950701.tsv" file contains 10000 log lines from one of NASA's apache server for July 1st, 1995.
+"in/nasa_19950801.tsv" file contains 10000 log lines for August 1st, 1995
+Create a Spark program to generate a new RDD which contains the hosts which are accessed on BOTH days.
+Save the resulting RDD to "out/nasa_logs_same_hosts.csv" file.
+
+Example output:
+vagrant.vf.mmc.com
+www-a1.proxy.aol.com
+.....    
+
+Keep in mind, that the original log files contains the following header lines.
+host    logname    time    method    url    response    bytes
+
+Make sure the head lines are removed in the resulting RDD.
+'''
