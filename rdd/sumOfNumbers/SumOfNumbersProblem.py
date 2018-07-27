66531baf9359a4bacb3f2836e1a64887314f3c6c
Hengyu Lou <hylou34@gmail.com>
2018-07-26 18:06:25 -0700

    Solve sum of numbers problem

--- rdd/sumOfNumbers/SumOfNumbersProblem.py
+++ rdd/sumOfNumbers/SumOfNumbersProblem.py
@@ -1,5 +1,5 @@
 import sys
-from pyspark import SparkContext
+from pyspark import SparkContext, SparkConf
 
 if __name__ == "__main__":
 
@@ -8,3 +8,12 @@ if __name__ == "__main__":
     print the sum of those numbers to console.
     Each row of the input file contains 10 prime numbers separated by spaces.
     '''
+
+    conf = SparkConf().setAppName("prime_sum").setMaster("local[*]")
+    sc = SparkContext(conf = conf)
+
+    lines = sc.textFile("in/prime_nums.text")
+    primes = lines.flatMap(lambda line: line.split("\t"))
+
+    total = primes.reduce(lambda x, y: int(x) + int(y))
+    print("sum is :{}".format(total)) 
